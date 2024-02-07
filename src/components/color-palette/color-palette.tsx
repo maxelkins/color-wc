b1eae2f57f745aadaec32a963f6f6f0865d3e64f
Max Elkins <hello@maxelkins.me>
2024-02-07 21:55:25 +0000

    Refinements

--- src/components/color-palette/color-palette.tsx
+++ src/components/color-palette/color-palette.tsx
@@ -16,43 +16,36 @@ export class ColorPalette {
     this.colorsArray = this.colors.split(',').map(color => color.trim());
   }
 
-  // Get the value of the custom property used for the background color
-  private customPropertyValue(color: string): string {
-    const hostStyle = getComputedStyle(this.hostElement);
-    return hostStyle.getPropertyValue(color).trim();
-  }
-
-  // Create backgroundColor custom property
-  private colorCusProp(color: string): string {
-    return 'var(' + color + ')';
-  }
-
   render() {
-    return this.colorsArray.map(color => {
-      const customPropValue = this.customPropertyValue(color);
+    const colorElements = this.colorsArray.map(color => {
+      const customPropValue = getComputedStyle(this.hostElement).getPropertyValue(color).trim();
       const HSLToHex = StringHSLToHex(customPropValue);
       const colorLuminance = getLuminance(HSLToHex);
       const whiteLuminance = 1;
       const blackLuminance = 0;
-      const contrastAgainstWhite = (Math.floor(getContrastRatio(colorLuminance, whiteLuminance) * 100) / 100).toFixed(2);
-      const contrastAgainstBlack = (Math.floor(getContrastRatio(colorLuminance, blackLuminance) * 100) / 100).toFixed(2);
+      const contrastAgainstWhite = getContrastRatio(colorLuminance, whiteLuminance).toFixed(2);
+      const contrastAgainstBlack = getContrastRatio(colorLuminance, blackLuminance).toFixed(2);
       const textColor = contrastAgainstBlack > contrastAgainstWhite ? 'black' : 'white';
+      const wcagBlack = wcagLevel(contrastAgainstBlack);
+      const wcagWhite = wcagLevel(contrastAgainstWhite);
 
       return (
-        <div class="colorPaletteRow" style={{ backgroundColor: this.colorCusProp(color) }}>
+        <div class="colorPaletteRow" style={{ backgroundColor: `var(${color})` }}>
           <div class="details">
             <span style={{ color: textColor }}>{customPropValue}</span>
           </div>
           <div class="a11y">
-            <span class={`a11y-black ${wcagLevel(contrastAgainstBlack) === 'Fail' ? 'a11y-fail' : 'a11y-pass'}`}>
-              {wcagLevel(contrastAgainstBlack)} {contrastAgainstBlack}
+            <span class={`a11y-black ${wcagBlack === 'Fail' ? 'a11y-fail' : 'a11y-pass'}`}>
+              {wcagBlack} {contrastAgainstBlack}
             </span>
-            <span class={`a11y-white ${wcagLevel(contrastAgainstWhite) === 'Fail' ? 'a11y-fail' : 'a11y-pass'}`}>
-              {wcagLevel(contrastAgainstWhite)} {contrastAgainstWhite}
+            <span class={`a11y-white ${wcagWhite === 'Fail' ? 'a11y-fail' : 'a11y-pass'}`}>
+              {wcagWhite} {contrastAgainstWhite}
             </span>
           </div>
         </div>
       );
     });
+
+    return colorElements;
   }
 }
