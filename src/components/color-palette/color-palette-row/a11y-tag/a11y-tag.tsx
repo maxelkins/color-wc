21e0024118089eeed01bf58e9274434cf6a50e5f
Max Elkins <hello@maxelkins.me>
2024-02-22 18:49:47 +0000

    Broke up tag into own component

---
+++ src/components/color-palette/color-palette-row/a11y-tag/a11y-tag.tsx
@@ -0,0 +1,62 @@
+import { Element, Component, Prop, h } from '@stencil/core';
+import { colord, extend } from 'colord';
+import a11yPlugin from 'colord/plugins/a11y';
+import labPlugin from 'colord/plugins/lab';
+import hwbPlugin from 'colord/plugins/hwb';
+import lchPlugin from 'colord/plugins/lch';
+
+extend([a11yPlugin, labPlugin, hwbPlugin, lchPlugin]);
+
+@Component({
+  tag: 'a11y-tag',
+  styleUrl: 'a11y-tag.css',
+  shadow: false,
+})
+export class ColorPaletteRow {
+  @Element() hostElement: HTMLElement;
+  @Prop() foreground: string;
+  @Prop() background: string;
+
+  render() {
+    const contrastValue: number = colord(this.foreground).contrast(this.background);
+    const wcagAAA: boolean = colord(this.foreground).isReadable(this.background, { level: 'AAA' });
+    const wcagAA: boolean = colord(this.foreground).isReadable(this.background, { level: 'AA' });
+    if (wcagAAA) {
+      return (
+        <span
+          style={{ backgroundColor: this.background }}
+          title={contrastValue.toString()}
+          class={`a11y-tag a11y-pass ${
+            this.background === '#000000' ? 'a11y-black' : 'a11y-white'
+          }`}
+        >
+          AAA <span class="a11y-contrast-value">{contrastValue}</span>
+        </span>
+      );
+    } else if (wcagAA) {
+      return (
+        <span
+          style={{ backgroundColor: this.background }}
+          title={contrastValue.toString()}
+          class={`a11y-tag a11y-pass ${
+            this.background === '#000000' ? 'a11y-black' : 'a11y-white'
+          }`}
+        >
+          AA <span class="a11y-contrast-value">{contrastValue}</span>
+        </span>
+      );
+    } else {
+      return (
+        <span
+          style={{ backgroundColor: this.background }}
+          title={contrastValue.toString()}
+          class={`a11y-tag a11y-fail ${
+            this.background === '#000000' ? 'a11y-black' : 'a11y-white'
+          }`}
+        >
+          Fail <span class="a11y-contrast-value">{contrastValue}</span>
+        </span>
+      );
+    }
+  }
+}
--- src/components/color-palette/color-palette-row/color-palette-row.tsx
+++ src/components/color-palette/color-palette-row/color-palette-row.tsx
@@ -16,48 +16,13 @@ export class ColorPaletteRow {
   @Element() hostElement: HTMLElement;
   @Prop() color: string;
 
-  private a11yTags(foreground, background) {
-    const contrastValue: number = colord(foreground).contrast(background);
-    const wcagAAA: boolean = colord(foreground).isReadable(background, { level: 'AAA' });
-    const wcagAA: boolean = colord(foreground).isReadable(background, { level: 'AA' });
-    if (wcagAAA) {
-      return (
-        <span
-          style={{ backgroundColor: background }}
-          title={contrastValue.toString()}
-          class={`a11y-tag a11y-pass ${background === '#000000' ? 'a11y-black' : 'a11y-white'}`}
-        >
-          AAA <span class="a11y-contrast-value">{contrastValue}</span>
-        </span>
-      );
-    } else if (wcagAA) {
-      return (
-        <span
-          style={{ backgroundColor: background }}
-          title={contrastValue.toString()}
-          class={`a11y-tag a11y-pass ${background === '#000000' ? 'a11y-black' : 'a11y-white'}`}
-        >
-          AA <span class="a11y-contrast-value">{contrastValue}</span>
-        </span>
-      );
-    } else {
-      return (
-        <span
-          style={{ backgroundColor: background }}
-          title={contrastValue.toString()}
-          class={`a11y-tag a11y-fail ${background === '#000000' ? 'a11y-black' : 'a11y-white'}`}
-        >
-          Fail <span class="a11y-contrast-value">{contrastValue}</span>
-        </span>
-      );
-    }
-  }
-
   render() {
     const colorElements = color => {
       const customPropValue: string = getComputedStyle(this.hostElement)
         .getPropertyValue(color)
         .trim();
+
+      // Text colour styling currently hardcoded to black or white
       const contrastAgainstWhite: number = colord(customPropValue).contrast('#ffffff');
       const contrastAgainstBlack: number = colord(customPropValue).contrast('#000000');
       const textColor: string = contrastAgainstBlack > contrastAgainstWhite ? 'black' : 'white';
@@ -73,8 +38,8 @@ export class ColorPaletteRow {
             <span style={{ color: textColor }}>{customPropValue}</span>
           </div>
           <div class="a11y">
-            {this.a11yTags(customPropValue, '#000000')}
-            {this.a11yTags(customPropValue, '#ffffff')}
+            <a11y-tag foreground={customPropValue} background="#000000"></a11y-tag>
+            <a11y-tag foreground={customPropValue} background="#ffffff"></a11y-tag>
           </div>
         </div>
       );
