17f3c7ab483140c220de4fbb0e0f150362dd7132
Max Elkins <hello@maxelkins.me>
2024-02-25 17:51:58 +0000

    Toggle styles

--- src/components/color-palette/color-palette-row/input-toggle/input-toggle.tsx
+++ src/components/color-palette/color-palette-row/input-toggle/input-toggle.tsx
@@ -15,7 +15,13 @@ export class InputToggle {
   render() {
     return (
       <label class="toggle" htmlFor={this.toggleId}>
-        <input checked={this.on || this.checked ? true : false} class="toggle-checkbox" type="checkbox" name={this.toggleId} id={this.toggleId} />
+        <input
+          checked={this.on || this.checked ? true : false}
+          class="toggle-checkbox"
+          type="checkbox"
+          name={this.toggleId}
+          id={this.toggleId}
+        />
         <span class="toggle-display" hidden>
           <svg
             aria-hidden="true"
@@ -27,7 +33,11 @@ export class InputToggle {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M6.08471 10.6237L2.29164 6.83059L1 8.11313L6.08471 13.1978L17 2.28255L15.7175 1L6.08471 10.6237Z" fill="currentcolor" stroke="currentcolor" />
+            <path
+              d="M6.08471 10.6237L2.29164 6.83059L1 8.11313L6.08471 13.1978L17 2.28255L15.7175 1L6.08471 10.6237Z"
+              fill="currentcolor"
+              stroke="currentcolor"
+            />
           </svg>
           <svg
             aria-hidden="true"
@@ -39,7 +49,10 @@ export class InputToggle {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M11.167 0L6.5 4.667L1.833 0L0 1.833L4.667 6.5L0 11.167L1.833 13L6.5 8.333L11.167 13L13 11.167L8.333 6.5L13 1.833L11.167 0Z" fill="currentcolor" />
+            <path
+              d="M11.167 0L6.5 4.667L1.833 0L0 1.833L4.667 6.5L0 11.167L1.833 13L6.5 8.333L11.167 13L13 11.167L8.333 6.5L13 1.833L11.167 0Z"
+              fill="currentcolor"
+            />
           </svg>
         </span>
         {this.toggleLabel}
